7d5ca4b6c2d1c27cafc532c009871fb65636574c
Maximilian <maximilianmarnold@gmail.com>
2025-12-04 15:37:12 +0100

    chore: Remove paperclip update logic in start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author: James A. Chambers - https://jamesachambers.com/
 # More information at https://jamesachambers.com/raspberry-pi-minecraft-server-script-with-startup-service/
-# GitHub Repository: https://github.com/TheRemote/RaspberryPiMinecraft
+# GitHub Repository: https://github.com/maxonary/RPi-Minecraft-Server
 # Minecraft Server startup script using screen -- view the console with screen -r minecraft
 
 # Set path variable
@@ -181,26 +181,6 @@ if [ -f "server.properties" ]; then
     sed -i "s/snooper-enabled=true/snooper-enabled=false/g" server.properties
     # Increase server watchdog timer to prevent it from shutting itself down without restarting
     sed -i "s/max-tick-time=60000/max-tick-time=120000/g" server.properties
-fi
-
-# Update paperclip.jar
-echo "Updating to most recent paperclip version ..."
-
-# Test internet connectivity first
-curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4.212 Safari/537.36" -s https://papermc.io/ -o /dev/null
-if [ "$?" != 0 ]; then
-    echo "Unable to connect to update website (internet connection may be down).  Skipping update ..."
-else
-    # Get latest build
-    BuildJSON=$(curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4.212 Safari/537.36" https://papermc.io/api/v2/projects/paper/versions/1.20.4)
-    Build=$(echo "$BuildJSON" | rev | cut -d, -f 1 | cut -d']' -f 2 | cut -d'[' -f 2 | rev)
-    Build=$(($Build + 0))
-    if [[ $Build != 0 ]]; then
-        echo "Latest paperclip build found: $Build"
-        curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4.212 Safari/537.36" -o paperclip.jar "https://papermc.io/api/v2/projects/paper/versions/1.20.4/builds/$Build/downloads/paper-1.20.4-$Build.jar"
-    else
-        echo "Unable to retrieve latest Paper build (got result of $Build)"
-    fi
 fi
 
 echo "Starting Minecraft server.  To view window type screen -r minecraft."
